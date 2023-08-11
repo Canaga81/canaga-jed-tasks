b03b157b89829d568d0b9ef194f0ef4c17980b18
Canaga81 <Canagamemmedov@gmail.com>
2023-08-11 09:49:29 +0400

    Task_1

---
+++ js/task_1/script.js
@@ -0,0 +1,6 @@
+let teref_1 = Number(prompt("Duzbucagin enini yaziniz(sm): "));
+let teref_2 = Number(prompt("Duzbucagin uzunlugunu yaziniz(sm): "));
+
+let sahe = teref_1 * teref_2;
+
+alert("Sahenin uzunlugu: " + sahe);
