e029ac4af1b76b48a86dc41036d7d0697b71ed16
Canaga81 <Canagamemmedov@gmail.com>
2023-08-11 09:57:23 +0400

    Task_2

---
+++ js/task_2/app.js
@@ -0,0 +1,12 @@
+let buIl = 2023;
+let user = Number(prompt("Doguldugunuz ili daxil edin: "));
+
+let zamanFerqi = buIl - user;
+
+if(zamanFerqi < 0) {
+    alert("Dogum ilinizi duzgun girin!");
+}
+
+else {
+    alert("Yasiniz: " + zamanFerqi);
+}
