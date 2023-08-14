6f158daa3de7835998d37783a49a8789a5623391
Canaga81 <Canagamemmedov@gmail.com>
2023-08-14 09:21:50 +0400

    Task_6

---
+++ js/task_6/hitler.js
@@ -0,0 +1,15 @@
+let eded_1 = Number(prompt("Eded Daxil Edin: "));
+let eded_2 = Number(prompt("Eded Daxil Edin: "));
+
+
+let pilus = eded_1 + eded_2;
+let minus = eded_1 - eded_2;
+let vurma = eded_1 * eded_2;
+let bolme = eded_1 / eded_2;
+
+
+console.log(`
+${eded_1} + ${eded_2} = ${pilus}
+${eded_1} - ${eded_2} = ${minus}
+${eded_1} * ${eded_2} = ${vurma}
+${eded_1} / ${eded_2} = ${bolme}`);
