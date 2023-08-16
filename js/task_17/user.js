2dfd129cce547e533313834127bc34f92f48eed1
Canaga81 <Canagamemmedov@gmail.com>
2023-08-16 11:17:33 +0400

    Task_17

---
+++ js/task_17/user.js
@@ -0,0 +1,15 @@
+let user = prompt("Soz yaziniz: ");
+
+
+if(user === "user") {
+    console.log("Xos Geldin Istifadeci :) ");
+}
+else if(user === "admin") {
+    console.log("Xos Geldin Cenab Admin :) ");
+}
+else if (user === "guest") {
+    console.log("Xos Geldin Qonaq :) ");
+}
+else {
+    console.log("Yazdiginiz Soz Yanlisdir !");
+}
