f489338bdd836485552f7e7975cd170b74bd4cb5
Canaga81 <Canagamemmedov@gmail.com>
2023-08-16 11:56:08 +0400

    Task_20

---
+++ js/task_20/script.js
@@ -0,0 +1,25 @@
+//! Yol 1
+
+
+let number_1 = +prompt("Eded Yaziniz: ");
+
+
+if(number_1 % 10 === 0) {
+    console.log("Bu Istedigim Ededdir !");
+}
+else {
+    console.log("Bu Istedigim Eded Deyil !");
+}
+
+
+//! Yol 2
+
+
+// let number_2 = +prompt("Eded Yaziniz: ");
+
+// if(number_2 % 2 === 0 && number_2 % 5 === 0 ) {
+//     console.log("Bu Istedigim Ededdir !");
+// }
+// else {
+//     console.log("Bu Istedigim Eded Deyil !");
+// }
