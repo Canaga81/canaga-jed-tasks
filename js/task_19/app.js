9a5e951f5a2b88909e8d18199ef9462229b1a5f7
Canaga81 <Canagamemmedov@gmail.com>
2023-08-16 11:47:36 +0400

    Task_19

---
+++ js/task_19/app.js
@@ -0,0 +1,43 @@
+//! Yol 1 (Yolun Ozu Sehvdi Ancaq Cavab Duzdu :) (prompt'u number'e cevirmeyende duz olur) )
+
+
+// let number = prompt("Eded Yaziniz: ");
+
+
+// if(number.length === 2) {
+//     console.log("Eded 2 reqemlidir !");
+// }
+// else {
+//     console.log("Eded 2 reqemli deyil !");
+// }
+
+
+//! Yol 2 (Esas Olan)
+
+
+let number_2 = +prompt("Eded Yaziniz: ");
+
+if(number_2 > 9) {
+    if(number_2 < 100) {
+        console.log("Eded Ikireqemlidir !");
+    }
+    else {
+        console.log("Eded Ikireqemli Deyil !");
+    }
+}
+else {
+    console.log("Eded Ikireqemli Deyil !");
+}
+
+
+//! Yol 3
+
+
+// let number_3 = +prompt("Eded Yaziniz: ");
+
+// if(number_3 > 9 && number_3 < 100) {
+//     console.log("Eded Ikireqemlidir !");
+// }
+// else {
+//     console.log("Eded Ikireqemli Degil");
+// }
