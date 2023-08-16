ec810671f7178d98d5ecee19d12aeee791a825fa
Canaga81 <Canagamemmedov@gmail.com>
2023-08-16 12:02:57 +0400

    Task_21

---
+++ js/task_21/game.js
@@ -0,0 +1,10 @@
+let num_1 = +prompt("1-ci Eded: ")
+let num_2 = +prompt("2-ci Eded: ")
+let num_3 = +prompt("3-cu Eded: ");
+
+if(num_1 + num_2 + num_3 > 100) {
+    console.log("Cemleri Kecdi 100-u");
+}
+else {
+    console.log("Cemleri Kece Bilmedi 100-u");
+}
