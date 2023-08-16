7956ce6156973423bd1520f0e85b829eaf9e855a
Canaga81 <Canagamemmedov@gmail.com>
2023-08-16 09:38:19 +0400

    Task_13

---
+++ js/task_13/number.js
@@ -0,0 +1,12 @@
+let number = +prompt("Eded yaziniz: ");
+
+
+if(number === 0) {
+    console.log("Eded 0-dir !");
+}
+else if(number > 0) {
+    console.log("Eded Musbetdir !");
+}
+else {
+    console.log("Eded Menfidir !");
+}
