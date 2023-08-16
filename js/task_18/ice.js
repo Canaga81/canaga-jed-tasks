753e72f71d7173940fde00fa5b807894e9f8404c
Canaga81 <Canagamemmedov@gmail.com>
2023-08-16 11:22:58 +0400

    Task_18

---
+++ js/task_18/ice.js
@@ -0,0 +1,15 @@
+let number = +prompt("Eded Yaziniz: ");
+
+
+if(number % 15 === 0) {
+    console.log("FizzBuzz");
+}
+else if(number % 3 === 0) {
+    console.log("Fizz");
+}
+else if(number % 5 === 0) {
+    console.log("Buzz");
+}
+else {
+    console.log("Hec Biride Deyil !");
+}
