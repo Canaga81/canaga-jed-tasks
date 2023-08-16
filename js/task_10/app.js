7349a0c4a7e87398dfb8f1bb098cd67701a5abb5
Canaga81 <Canagamemmedov@gmail.com>
2023-08-16 09:20:33 +0400

    Task_10

---
+++ js/task_10/app.js
@@ -0,0 +1,21 @@
+let chooseMusic = prompt(`Sevdiyin Musiqi Adini Yaz: 
+1. Cennetten Cicek
+2. Issiz Duvarlar
+3. Fifty-Fifty
+4. Bakili Balasiyam`)
+
+if(chooseMusic === "1") {
+    console.log("Acilara Yuruyor Korkmuyorum");
+}
+else if(chooseMusic === "2") {
+    console.log("Gunumu,Gunesimi zifiri yapan dunya...");
+}
+else if(chooseMusic === "3") {
+    console.log("la la la laa");
+}
+else if(chooseMusic === "4") {
+    console.log("Bakili balasiyam, Bakida dogulmusam,Bakida qalasiyam");
+}
+else {
+    console.log("Bu reqemde mahni yoxdur !");
+}
