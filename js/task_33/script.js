596fe291738eeaa1b208d83a2c4a63e30ee7e373
Canaga81 <Canagamemmedov@gmail.com>
2023-08-25 12:44:09 +0400

    Task_33

---
+++ js/task_33/script.js
@@ -0,0 +1,12 @@
+let eded = +prompt("Eded Daxil Edin: ");
+let sum = 0;
+let step = 0
+
+for(i=100; i<1000; i++) {
+    if(i % eded === 0) {
+        sum += i;
+        step++;
+    }
+}
+
+console.log(sum + " " + step);
