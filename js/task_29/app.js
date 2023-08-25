2eff1aac211261355d78b843b756bddad65d761c
Canaga81 <Canagamemmedov@gmail.com>
2023-08-25 12:11:35 +0400

    Task_29

---
+++ js/task_29/app.js
@@ -0,0 +1,19 @@
+//! Yol 1
+
+// let ad = prompt("Adnizi Daxil Edin: ");
+// let program = prompt("Bildiyiniz Programlasdirma Dilini Daxil Edin: ")
+
+// function logGreeting() {
+//     console.log(`${ad} is a ${program} developer`);
+// }
+
+// logGreeting();
+
+
+//! Yol 2
+
+function logGreeting(ad, program) {
+    console.log(`${ad} is a ${program} developer`);
+}
+
+logGreeting('Canaga', 'JS');
