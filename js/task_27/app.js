ac6c9764e9b4ce2195910b8edaffdb445dc1d901
Canaga81 <Canagamemmedov@gmail.com>
2023-08-25 11:28:27 +0400

    Task_27

---
+++ js/task_27/app.js
@@ -0,0 +1,14 @@
+function twoNumber(){
+    let sum = 0;
+    let step = 0;
+
+    for(let i=14; i<100; i+=7) {
+        sum += i;
+        step++;
+    }
+
+    console.log(`7-e Bolunen Butun 2 reqemli ededlerin cemi ${sum}
+Iterasiya Sayi ise ${step} oldu`);
+}
+
+twoNumber()
