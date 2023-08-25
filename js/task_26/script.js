6f8f6542e3f093cf9ba8bb17f3d478153fbc96ee
Canaga81 <Canagamemmedov@gmail.com>
2023-08-25 11:19:14 +0400

    Task_26

---
+++ js/task_26/script.js
@@ -0,0 +1,9 @@
+let ed1 = +prompt("Eded Daxil Edin: ");
+let ed2 = +prompt("Eded Daxil Edin: ");
+let ed3 = +prompt("Eded Daxil Edin: ");
+
+function findMax(ed1, ed2, ed3) {
+    console.log(`Verilen Ededlerden En Boyuyu:${Math.max(ed1, ed2, ed3)}`);
+}
+
+findMax(ed1, ed2, ed3);
