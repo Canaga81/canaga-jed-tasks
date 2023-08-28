ccfa81b7fc293cc7201d5e6a882dd41efa4b8de0
Canaga81 <Canagamemmedov@gmail.com>
2023-08-28 10:07:54 +0400

    Task_37

---
+++ js/task_37/app.js
@@ -0,0 +1,9 @@
+let eded_1 = +prompt("Eded Daxil Edin");
+let eded_2 = +prompt("Eded Daxil Edin");
+
+function Numbers(eded_1, eded_2) {
+    console.log(`En Kicik Eded: ${Math.min(eded_1, eded_2)}`);
+}
+
+
+Numbers(eded_1, eded_2);
