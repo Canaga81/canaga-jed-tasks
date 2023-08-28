1ac4ae6821a6d8acf4195e768a05c41381b2fd94
Canaga81 <Canagamemmedov@gmail.com>
2023-08-28 12:04:37 +0400

    Task_41

---
+++ js/task_41/app.js
@@ -0,0 +1,12 @@
+let eded = +prompt("Eded daxil Edin: ");
+
+function isEven(eded) {
+    if(eded % 2 === 0) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+}
+
+isEven(eded)
