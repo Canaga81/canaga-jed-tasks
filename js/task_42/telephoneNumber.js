9cc114ff5cf42098486b9928534f72e7f3a2b467
Canaga81 <Canagamemmedov@gmail.com>
2023-08-28 13:17:12 +0400

    Task_42

---
+++ js/task_42/telephoneNumber.js
@@ -0,0 +1,15 @@
+function isvalidnumber(phoneNumber) {
+    if (phoneNumber.length === 11 && phoneNumber.charAt(0) === "+7") {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+// Test
+
+const phoneNumber = "+79123456789";
+
+isvalidnumber(phoneNumber)  // true
+
+//! Yaza Bilmedim :(
