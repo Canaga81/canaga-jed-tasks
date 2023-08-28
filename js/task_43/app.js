961f829d45faa1f3ad7dae807b9a1ed6e9a279f9
Canaga81 <Canagamemmedov@gmail.com>
2023-08-28 12:27:21 +0400

    Task_43

--- js/task_43/app.js
+++ js/task_43/app.js
@@ -1,12 +1,16 @@
 let metn = "Paris cox gozel seherdir";
 
-let metnArr = metn.split('');
-let sum = 0;
+function numCounter(metn) {
+    let metnArr = metn.split('');
+    let sum = 0;
 
-metnArr.forEach((herf) => {
-    if((/[a-zA-Z0-9]/).test(herf)) {
-        sum += 1;
-    }
-});
+    metnArr.forEach((herf) => {
+        if ((/[a-zA-Z0-9]/).test(herf)) {
+            sum += 1;
+        }
+    });
 
-console.log(sum);
+    console.log(sum);
+}
+
+numCounter(metn)
