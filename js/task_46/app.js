8eb3110a24c2f3a7c4d7f54208b96a13242adb92
Canaga81 <Canagamemmedov@gmail.com>
2023-08-30 10:33:15 +0400

    Task_46

--- js/task_46/app.js
+++ js/task_46/app.js
@@ -1,25 +1,42 @@
-const existsAndTruthy = () => {
+//! Yol 1
 
-    const obj = {
-        a:1,
-        b:2,
-        c:null,
-        d:undefined
-    }
+// const existsAndTruthy = () => {
 
-    for(let key in obj) {
-        if(obj[key] === null) {
-            console.log(false);
-        }
-        else if(obj[key] === undefined) {
-            console.log(false);
-        }
-        else {
-            console.log(true);
-        }
+//     const obj = {
+//         a:1,
+//         b:2,
+//         c:null,
+//         d:undefined
+//     }
+
+//     for(let key in obj) {
+//         if(obj[key] === null) {
+//             console.log(false);
+//         }
+//         else if(obj[key] === undefined) {
+//             console.log(false);
+//         }
+//         else {
+//             console.log(true);
+//         }
         
-    }
+//     }
 
+// }
+
+// existsAndTruthy()
+
+//! Yol 2
+
+const existsAndTruthy = (obj, propertyName) => {
+    if(obj[propertyName]) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-existsAndTruthy()
+console.log(existsAndTruthy({a:1,b:2,c:3},'b')); // true
+console.log(existsAndTruthy({x:'a',y:null,z:'c'},'y'));  // false (obyektde 'y' var amma falsy-di ona gore false)
+console.log(existsAndTruthy({x:'a',b:'b',z:undefined},'z'));  // false  (obyektde 'z' yoxdu ona gore false)
