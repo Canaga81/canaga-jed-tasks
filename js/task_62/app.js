32c4749f203d81f18817ebc86e27f1b3f9b23811
Canaga81 <Canagamemmedov@gmail.com>
2023-09-04 14:23:15 +0400

    Task_62

---
+++ js/task_62/app.js
@@ -0,0 +1,21 @@
+//! 8.Telefon nömrəsini parametr kimi qəbul edən ısvalidnumber() funksiyasını yazın. 
+//! Telefon nömrəsi 11 rəqəmdən ibarət olduqda və +7 ilə başladıqda, funksiya true qaytarır.
+
+function isvalidNumber(number) {
+    if(number.length === 12) {
+        if(number.startsWith('+7') ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(isvalidNumber('+71234567890'));
+// console.log(isvalidNumber('+71234567895'));
+// console.log(isvalidNumber('71234567895'));
+// console.log(isvalidNumber('+51234567895'));
