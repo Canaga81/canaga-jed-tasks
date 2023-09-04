8ca6d48f9f1e4472c3c969ce07a00427e3d07428
Canaga81 <Canagamemmedov@gmail.com>
2023-09-04 16:47:49 +0400

    Task_54

--- js/task_54/app.js
+++ js/task_54/app.js
@@ -1,5 +1,15 @@
-let str = "Alion";
+// let str = "Alion";
 
-let netice = str.replace(str[0], '');
+// let netice = str.replace(str[0], '');
 
-console.log(netice);
+// console.log(netice);
+
+// 3. removeCharacterFromString("Alion","A") -> "lion" . bele bir funksiya yazin, sozden character silme. Alion sozu verdik ve A characteri verdik, sozden A-ni sildi. 
+
+function removeCharacterFromString(soz,herf) {
+
+    return soz.slice(0,soz.indexOf(herf)) + soz.slice(soz.indexOf(herf)+1,soz.length)
+    
+}
+
+console.log(removeCharacterFromString("Alion","A"));
