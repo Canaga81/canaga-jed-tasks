4ac386b133fd4b7dc92b126679967462bf703ad3
Canaga81 <Canagamemmedov@gmail.com>
2023-09-04 11:46:46 +0400

    Task_59

---
+++ js/task_59/app.js
@@ -0,0 +1,33 @@
+let eded = +prompt("Eded Daxil Edin: ");
+
+function isLucky(eded) {
+
+    let sonEded = eded % 10;
+    eded = Math.floor(eded / 10);
+
+    let besinciEded = eded % 10;
+    eded = Math.floor(eded / 10);
+
+    let dorduncuEded = eded % 10;
+    eded = Math.floor(eded / 10);
+
+    let ucuncuEded = eded % 10;
+    eded = Math.floor(eded / 10);
+
+    let ikinciEded = eded % 10;
+    eded = Math.floor(eded / 10);
+
+    let birinciEded = eded % 10;
+    eded = Math.floor(eded / 10);
+
+
+    if (sonEded + besinciEded + dorduncuEded === ucuncuEded + ikinciEded + birinciEded) {
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+
+}
+
+isLucky(eded);
