2f2909934c2231818876bb0eef8a024043005cd7
Canaga81 <Canagamemmedov@gmail.com>
2023-09-04 10:10:22 +0400

    Task_53

---
+++ js/task_53/app.js
@@ -0,0 +1,7 @@
+//! 1. Write a JavaScript function to split a sentence (JS funksiyasi yazin, cumle verilir, cumledeki sozleri ayirsin).
+
+let str = 'Bu,gun,hava,cox,istidir';
+
+let netice = str.split(',');
+
+console.log(netice);
