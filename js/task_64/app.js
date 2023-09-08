b0a6227c149570b9a9ccf6042f1514ffe3a6a4aa
Canaga81 <Canagamemmedov@gmail.com>
2023-09-08 10:07:59 +0400

    Task_64

---
+++ js/task_64/app.js
@@ -0,0 +1,12 @@
+let uzunlug = 5;
+
+let massiv = [];
+
+for(let i=0; i<uzunlug; i++) {
+    let reqem = parseInt(prompt("Reqem Daxil Edin: " + (i+1)));
+
+    massiv.push(reqem);
+
+}
+
+console.log(massiv);
