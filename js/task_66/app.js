479e34707d25ac2c4e56936040368f5fbae611fc
Canaga81 <Canagamemmedov@gmail.com>
2023-09-08 10:40:43 +0400

    Task_66

---
+++ js/task_66/app.js
@@ -0,0 +1,37 @@
+//! Yol-1
+
+function multiply(arr) {
+
+    let hasil = 1;
+
+    for(const reqem of arr) {
+
+        hasil *= reqem;
+
+    }
+
+    return hasil;
+
+}
+
+console.log(multiply([1, 2, 3]));
+console.log(multiply([1, 2, 3, 6, 3, 9]));
+
+//! Yol-2
+
+// function multiply(arr) {
+
+//     let hasil = 1;
+
+//     for(let i=0; i<arr.length; i++) {
+
+//         hasil *= arr[i];
+
+//     }
+
+//     return hasil;
+
+// }
+
+// console.log(multiply([1, 2, 3]));
+// console.log(multiply([1, 2, 3, 6, 3, 9]));
