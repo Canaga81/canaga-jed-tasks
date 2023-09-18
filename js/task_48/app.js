c87a531db8f7198873ff6da67746508e320ad850
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-09-18 14:02:45 +0400

    live legend 🐱‍💻

--- js/task_48/app.js
+++ js/task_48/app.js
@@ -1,3 +1,4 @@
+// A: live legend 🐱‍💻
 //! Car constructor-u yaradin. Biz hemin constructor-u cagiranda onun modelini, 
 //! istehsal ilini, sahibini verek. Ve elbette, masinin surmek, tormozlamaq kimi 
 //! methodlari olmalidir ki, onlari da verin. Meselen, surmek methodunu cagirsam ekrana,  
