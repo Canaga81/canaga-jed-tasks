6b75e04628746978d22df2457f23ba3138a7cccd
Alion-GreenHeart <56336368+Ali-GreenHeart@users.noreply.github.com>
2023-09-18 14:02:56 +0400

    live legend 🐱‍💻

--- js/task_56/app.js
+++ js/task_56/app.js
@@ -1,3 +1,4 @@
+// A: 
 //! 1.İstifadəçidən ikirəqəmli ədəd daxil etməsini xahiş edin. Bu rəqəmi üçrəqəmli olana qədər, 7 artırın. Son dəyəri konsola çıxarın.
 
 let userNumber = +prompt("Iki Reqemli Eded Daxil Edin: ");
