c4febd28e24a1439118ea6479f9a805b17e98544
Canaga81 <Canagamemmedov@gmail.com>
2023-09-18 12:54:13 +0400

    Task_87.3

--- js/task_87/app.js
+++ js/task_87/app.js
@@ -1,66 +1,72 @@
-const users=[
+const users = [
     {
-        id:1, 
-        username:"Ali",
-        img:'images/ali.jpg',
-        job:'developer'
+        id: 1,
+        username: "Ali",
+        img: 'images/ali.jpg',
+        job: 'developer'
     },
     {
-        id:2,
-        username:'bedbext_developer',
-        img:'images/bedbext_developer.jpg',
-        job:'test2',
+        id: 2,
+        username: 'bedbext_developer',
+        img: 'images/bedbext_developer.jpg',
+        job: 'test2',
     },
     {
-        id:3,
-        username:'duman',
-        img:'images/duman.jpg',
-        job:'test3',
+        id: 3,
+        username: 'duman',
+        img: 'images/duman.jpg',
+        job: 'test3',
     },
     {
-        id:4,
-        username:'js_oyrenen_shexs',
-        img:'images/js_oyrenen_shexs.jpg',
-        job:'test4',
+        id: 4,
+        username: 'js_oyrenen_shexs',
+        img: 'images/js_oyrenen_shexs.jpg',
+        job: 'test4',
     },
     {
-        id:5,
-        username:'qarli_daglar',
-        img:'images/qarli_daglar.jpg',
-        job:'test5',
+        id: 5,
+        username: 'qarli_daglar',
+        img: 'images/qarli_daglar.jpg',
+        job: 'test5',
     },
     {
-        id:6,
-        username:'qoca_cinar',
-        img:'images/qoca_cinar.jpg',
-        job:'test6',
+        id: 6,
+        username: 'qoca_cinar',
+        img: 'images/qoca_cinar.jpg',
+        job: 'test6',
     },
     {
-        id:7,
-        username:'visselka',
-        img:'images/visselka.jpg',
-        job:'test7',
+        id: 7,
+        username: 'visselka',
+        img: 'images/visselka.jpg',
+        job: 'test7',
     },
     {
-        id:8,
-        username:'novxani',
-        img:'images/novxani.jpg',
-        job:'test8',
+        id: 8,
+        username: 'novxani',
+        img: 'images/novxani.jpg',
+        job: 'test8',
     },
 ]
-const root = document.getElementById('root');
-const rootSection = document.querySelector('.root-section')
-//   your code goes here
+
+const rootEl = document.getElementById('root');
+const rootSec = document.querySelector('.root-section');
 
 users.forEach((user) => {
-    const userDiv = document.createElement('div');
-    userDiv.className = 'card';
+    const card = document.createElement('div');
+    card.className = 'card';
 
-    userDiv.innerHTML = `
+    card.innerHTML = `
+        <div class="image">
+            <img src="${user.img}" alt="card-image">
+        </div>
+
+        <div class="text">
             <h2>${user.username}</h2>
             <p>${user.job}</p>
+        </div>
     `;
 
-    rootSection.appendChild = userDiv;
-    root.appendChild = rootSection;
-})
+    rootSec.appendChild(card);
+    rootEl.appendChild(rootSec)
+});
